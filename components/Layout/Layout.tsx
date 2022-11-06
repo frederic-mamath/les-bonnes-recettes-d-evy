c9e4cb8498f3824c146a4a91ec3984d76560715d
Frederic MAMATH <frederic.mamath@eurex.com>
2022-11-06 10:28:15 +0100

    :package: Adds mui

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Button, Stack, Typography } from '@mui/material'
+import { ReactNode } from 'react'
+
+interface Props {
+    children: ReactNode
+}
+
+const Layout = (props: Props) => {
+    const { children } = props
+
+    return (
+        <div>
+            <AppBar>
+                <Stack direction="row">
+                    <Button variant="contained">Home</Button>
+                    <Typography variant="h1">Les bonnes recettes d&apos;evy</Typography>
+                </Stack>
+            </AppBar>
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout;
